09f30b4d90d979febab3fa5ae599c99d26f6aafa
Jayakrishnan <jayu2442@gmail.com>
2020-10-26 01:36:09 +0530

    Added sample_app_0.1

---
+++ task-07/sample_ui/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:sample_ui/views/custom_page_view.dart';
+import 'package:sample_ui/widgets/account_button_widget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ubiquitous UI',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: TextTheme(
+              button: TextStyle(fontSize: 18.0, color: Colors.white))),
+      home: Scaffold(
+        
+        body: DefaultTabController(
+          length: 2,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                  child: CustomPageView(),
+                ),
+                flex: 25,
+              ),
+              Expanded(
+                child: Container(
+                  child: AccountButtonWidget(),
+                ),
+                flex: 3,
+              )
+            ],
+          ),
+        ),
+        
+      ),
+    );
+  }
+}
