bbb2bee56882040ac2b2dca9969e3ecd69c24b33
Jayakrishnan Jayu <jaravindakshan@4labsinc.com>
2020-11-26 16:19:30 +0530

    Update code - use page argument for pagination

--- task-08/main.py
+++ task-08/main.py
@@ -5,39 +5,31 @@ import json
 user = 'snitch3s'
 #user = 'opendistro-for-elasticsearch'
 url = 'https://api.github.com/users/'+user+'/repos'
-hasNext = True
+page = 1
 counter = 0
-while (hasNext):
+while (True):
 
-    response = requests.get(url)
-
-    try:
-        head = response.headers['link']
-        if ('rel="prev"' in head.split(";")[1]):
-            hasNext = False
-        else:
-            url = head.split(";")[0][1:-1]
-    except KeyError:
-        print("No other pages found")
-        hasNext = False
-    finally:
-        commits = ""
+    response = requests.get(url+'?page='+str(page))
+    if not response.json():
+        break
+    page += 1
+    commits = ""
         
-        for repo in response.json():
-            counter+=1
-            print(counter , "searching inside "+ repo["name"])
+    for repo in response.json():
+        counter+=1
+        print(counter , "searching inside "+ repo["name"])
             
-            result = subprocess.run(['perceval', 'git', '--json-line','https://github.com/'+user+'/'+ repo['name']], stdout=subprocess.PIPE)
+        result = subprocess.run(['perceval', 'git', '--json-line','https://github.com/'+user+'/'+ repo['name']], stdout=subprocess.PIPE)
 
-            output = result.stdout.decode('utf-8')
+        output = result.stdout.decode('utf-8')
 
-            for data in output.split("\n")[:-1]:
+        for data in output.split("\n")[:-1]:
 
-                json_data = json.loads(data)
-                commits += str(json_data) + '\n'
+            json_data = json.loads(data)
+            commits += str(json_data) + '\n'
 
-                print(json_data['data']['message'])
+            print(json_data['data']['message'])
 
 
-        with open("commits.json", "w") as myFile:
-            myFile.write(commits)
+    with open("commits.json", "w") as myFile:
+        myFile.write(commits)
